8ba0ade21ed085ef3df85fbb2cb49678eef4c828
Charlie_egpu <tchoi.tab@gmail.com>
2022-03-06 14:55:56 +0900

    :sparkles: update

--- src/custom.d.ts
+++ src/custom.d.ts
@@ -4,3 +4,14 @@ declare module '*.svg' {
   // const content: (props: SVGProps<SVGElement>) => ReactElement;
   export default content;
 }
+
+declare module '*.png' {
+  const value: any;
+  export default value;
+}
+
+declare module '*.jpg' {
+  const value: any;
+  export default value;
+}
+
